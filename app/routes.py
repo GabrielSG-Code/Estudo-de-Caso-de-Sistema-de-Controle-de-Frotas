46bc330454d01d4532fec7166f510289ce832263
GabrielSG-Code <gabrielsapucaia@outlook.com>
2026-03-06 18:17:12 -0300

    feat: conectando a rota veiculos ao banco de dados e ao __init__

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import current_app as app # Importa o app que está rodando
 from flask import render_template
+from .models import Veiculo
 
 @app.route('/') # o símbolo "@" significa "Decorador" - Basicamente está dizendo: "Flask, fique de olho na porta principal (/). Se alguém entrar, execute a função abaixo."
 def index():
@@ -7,4 +8,9 @@ def index():
 
 @app.route('/veiculos')
 def listar_veiculos():
-    return "<h3>Aqui aparecerá a lista de veículos da sua planilha em breve!</h3>"
+
+    # Buscamos todos os veículos do banco de dado
+    lista_de_veiculos = Veiculo.query.all()
+
+    # Passamos a lista para o HTML através da variável 'veiculos'
+    return render_template('ConsultaDeVeiculos.html', veiculos = lista_de_veiculos)
