ed496649bae922ac2115c2fd85dd56c1640bbb39
GabrielSG-Code <gabrielsapucaia@outlook.com>
2026-03-06 18:20:13 -0300

    feat: caminho adicionado para o python reconhecer o caminho, além da biblioteca Os, cujo identifica o sistema e faz a conversão automaticamente da "/"

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,19 @@
 from flask import Flask
 from .models import db
+import os
 
 def create_app():
     app = Flask(__name__)
 
+    
+    ## Isso descobre o caminho da pasta onde este arquivo __init__.py está
+    basedir = os.path.abspath(os.path.dirname(__file__))
+
+    ## Isso sobe um nível e entra na pasta 'instance', garantindo o caminho correto
+    db_path = os.path.join(basedir, '..', 'instance', 'frota.db')
+
     # Configuração do Banco de Dados (SQLLite para começar, que cria um arquivo local)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///frota.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_path
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
