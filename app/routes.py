d7fb1a46dab429dcd396b9b85304a3b5f798eafc
GabrielSG-Code <gabrielsapucaia@outlook.com>
2026-03-11 15:39:18 -0300

    feat: adição do modal detalhes que mostra todos os detalhes do veiculo

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,11 @@
 from flask import current_app as app # Importa o app que está rodando
-from flask import render_template
+from flask import render_template, jsonify
 from .models import Veiculo
+from flask_login import LoginManager
+from wtforms import validators
+
+
+#login_manager = LoginManager()
 
 @app.route('/') # o símbolo "@" significa "Decorador" - Basicamente está dizendo: "Flask, fique de olho na porta principal (/). Se alguém entrar, execute a função abaixo."
 def index():
@@ -14,3 +19,23 @@ def listar_veiculos():
 
     # Passamos a lista para o HTML através da variável 'veiculos'
     return render_template('ConsultaDeVeiculos.html', veiculos = lista_de_veiculos)
+
+@app.route('/api/veiculo/<string:placa>')
+def api_detalhe_veiculo(placa):
+    veiculo = Veiculo.query.get_or_404(placa)
+    dados = {
+        "placa": veiculo.placa,
+        "modelo": veiculo.modelo,
+        "marca": veiculo.marca,
+        "ano": veiculo.ano,
+        "combustivel": veiculo.combustivel.value,
+        "Km_atual": veiculo.Km_Atual,
+        "Km_UltimaTrocaOleo": veiculo.Km_UltimaTrocaOleo,
+        "Km_UltimaRevisao": veiculo.Km_UltimaRevisao,
+        "Data_ultimaRevisao": veiculo.Data_ultimaRevisao.strftime('%d/%m/%Y'),
+        "status": veiculo.status.value
+    }
+    return jsonify(dados)
+
+@app.route('/login', methods=['POST'])
+def login_user():
